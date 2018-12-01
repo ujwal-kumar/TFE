e18901f2d0fc8ed1a7ddbabe2039972ad21dec9e
ujwal-kumar <ujwal.prodevans@gmail.com>
2018-12-01 15:23:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "aws_security_group" "sgweb" {
 resource "aws_instance" "wb" {
    ami  = "${var.ami}"
    instance_type = "t2.micro"
-   key_name = "${aws_key_pair.default.id}"
+   key_name = "${var.key_name}"
    subnet_id = "${aws_subnet.public-subnet.id}"
    vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
    associate_public_ip_address = true
