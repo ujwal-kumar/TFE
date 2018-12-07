1cad58ddf9212f094564ef5ce9063bb2af20d2df
ujwal-kumar <ujwal.prodevans@gmail.com>
2018-12-07 17:16:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,28 +48,28 @@ resource "aws_security_group" "sgweb" {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = ["106.51.75.18/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port = 443
     to_port = 443
     protocol = "tcp"
-    cidr_blocks = ["106.51.75.18/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port = -1
     to_port = -1
     protocol = "icmp"
-    cidr_blocks = ["106.51.75.18/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
     
   ingress {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks =  ["106.51.75.18/32"]
+    cidr_blocks =  ["0.0.0.0/0"]
   }
     
   ingress {
